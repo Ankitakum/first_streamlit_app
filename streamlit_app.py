6c7224ef731b28cd5f2994121d429392e2de23b0
Ankitakum <115623814+Ankitakum@users.noreply.github.com>
2022-10-12 16:47:25 +0530

    Update streamlit_app.py
    Added Title and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My Parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal ')
+streamlit.text('Kale, Spaniach & Rocket Smoothie')
+streamlit.text('Hard-Boiled free-Range Egg')
